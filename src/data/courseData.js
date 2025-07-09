53fe6c329c30ac6483fcc5238f17defe774f9ef8
Zaplavs <Zaplavs@yandex.ru>
2025-07-09 11:57:08 +0300

    раскинул данные о подборках сайта в отдельный файл

---
+++ src/data/courseData.js
@@ -0,0 +1,138 @@
+// src/data/courseData.js
+
+const courseData = {
+  1: {
+    title: 'Python',
+    courses: [
+      {
+        name: '"Поколение Python": курс для начинающих',
+        link: 'https://stepik.org/course/58852/syllabus ',
+        description: '"Поколение Python: курс для начинающих" знакомит с основными типами данных, конструкциями и принципами структурного программирования языка Python.',
+        image: 'https://cdn.stepik.net/media/cache/images/courses/58852/cover_ZP5DuRM/4951fdb332a542d0a2fe70692976e557.png '
+      },
+      {
+        name: '"Поколение Python": курс для продвинутых',
+        link: 'https://stepik.org/course/68343/promo ',
+        description: 'Курс знакомит с такими возможностями языка Python, как двумерные массивы, кортежи, множества, словари и многое другое.',
+        image: 'https://cdn.stepik.net/media/cache/images/courses/68343/cover_xAgB0QZ/e1ec4a8a7903731d611952d63b4b782f.png '
+      },
+      {
+        name: 'Инди-курс программирования на Python',
+        link: 'https://stepik.org/course/63085/syllabus ',
+        description: 'Добро пожаловать на «Инди-курс программирования на Python» — курс, который стал выбором тысяч учеников и продолжает развиваться вместе с миром python!',
+        image: 'https://cdn.stepik.net/media/cache/images/courses/63085/cover_eig1Jqm/1e06c59e71b23ef2294f0bfea1a6e118.jpg '
+      }
+    ]
+  },
+  2: {
+    title: 'SQL',
+    courses: [
+      {
+        name: 'Интерактивный тренажёр по SQL',
+        link: 'https://stepik.org/course/63054/info ',
+        description: 'В курсе большинство шагов — это практические задания на создание SQL-запросов.',
+        image: 'https://cdn.stepik.net/media/cache/images/courses/63054/cover_foIuz1t/6bc976a3abd69e9e3e5163a5973a8ccf.jpg '
+      },
+      {
+        name: 'SQLtest.online',
+        link: 'https://sqltest.online/ ',
+        description: 'Бесплатный онлайн-тренажёр для практики SQL-запросов прямо в браузере.',
+        image: 'https://habrastorage.org/r/w780/getpro/habr/upload_files/6a5/2f3/b75/6a52f3b7557ce14f79c4ba784406dbc8.png '
+      },
+      {
+        name: 'SQL Tutorial, SQLZoo',
+        link: 'https://sqlzoo.net/wiki/SQL_Tutorial ',
+        description: 'Бесплатный ресурс с интерактивными уроками и практическими задачами.',
+        image: 'https://sqlzoo.net/static/malones.svg '
+      }
+    ]
+  },
+  3: {
+    title: 'JavaScript',
+    courses: [
+      {
+        name: 'JavaScript.info',
+        link: 'https://javascript.info/ ',
+        description: 'Современный учебник по JavaScript от основ до продвинутых тем.',
+        image: 'https://javascript.info/img/site-logo.svg '
+      },
+      {
+        name: 'freeCodeCamp JavaScript',
+        link: 'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/ ',
+        description: 'Бесплатные курсы и практика алгоритмов на JavaScript.',
+        image: 'https://placehold.co/300x200?text=fcc+JS'
+      },
+      {
+        name: 'W3Schools JS Tutorial',
+        link: ' https://www.w3schools.com/js/ ',
+        description: 'Классический справочник и руководство по JavaScript.',
+        image: 'https://placehold.co/300x200?text=W3Schools+JS'
+      }
+    ]
+  },
+  4: {
+    title: 'React',
+    courses: [
+      {
+        name: 'React Official Docs',
+        link: ' https://react.dev/learn ',
+        description: 'Официальная документация React. Лучший источник знаний о библиотеке.',
+        image: 'https://react.dev/images/branding/react-vertical-white-2023.svg '
+      },
+      {
+        name: 'freeCodeCamp React',
+        link: 'https://www.freecodecamp.org/learn/front-end-development-libraries/#sass:~:text=9%20challenges%20completed-,React,-React%20is%20a',
+        description: 'Курс за час: всё самое важное о React для начинающих.',
+        image: ' https://placehold.co/300x200?text=fcc+React'
+      },
+      {
+        name: 'React Tutorial w3schools',
+        link: ' https://www.w3schools.com/REACT/DEFAULT.ASP ',
+        description: 'курс для начинающих разработчиков на React.',
+        image: 'https://placehold.co/300x200?text=Wes+Bos'
+      }
+    ]
+  },
+  5: {
+    title: 'Data Science',
+    courses: [
+      {
+        name: 'Kaggle Learn',
+        link: ' https://www.kaggle.com/learn ',
+        description: 'Бесплатные курсы по машинному обучению и анализу данных.',
+        image: 'https://storage.googleapis.com/kaggle-datasite/images/Kaggle_Logo_2022.png '
+      },
+      {
+        name: 'Введение в Data Science и машинное обучение',
+        link: 'https://stepik.org/course/4852/info ',
+        description: 'Познакомимся с такими методами машинного обучения как деревья решений и нейронные сети.',
+        image: 'https://cdn.stepik.net/media/cache/images/courses/4852/cover_R0OtNuB/e51489363d079b8830367935563887e8.jpg '
+      },
+      {
+        name: 'Нейронные сети',
+        link: 'https://stepik.org/course/401/promo ',
+        description: 'В рамках данного курса слушатели познакомятся с теоретическими и практическими основами искусственных нейронных сетей.',
+        image: ''
+      }
+    ]
+  },
+  6: {
+    title: 'DevOps',
+    courses: [
+      {
+        name: 'курс от канала «Из сисадмина в DevOps»',
+        link: 'https://www.youtube.com/playlist?list=PLQoP6S9f51EZM0-WqAWAAkwAB28gnWkTb',
+        description: 'Изучение основного стека',
+        image: ''
+      },
+      {
+        name: 'курсы от канала «Cisco Ne Slabo / Sedicomm TV»',
+        link: ' https://www.youtube.com/playlist?list=PLMiVLClzZDbSQTrwRHSeGR6H7vcsv7WuS',
+        description: 'Изучение основного стека',
+        image: ''
+      }
+    ]
+  }
+};
+
+export default courseData;
